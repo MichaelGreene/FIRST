b80ba8b12a97949907ac3a587d8e585ddc079899
Michael Greene <mkg190@gmail.com>
2015-05-08 00:53:56 -0400

    initial c file and test cpp file

---
+++ famA.cpp
@@ -0,0 +1,18 @@
+/* WORK! */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+char adj[5][12];
+char noun[5][12];
+
+int main() {
+	cout << "Hello World!\n";
+	cout << "Enter the first adjective\n";
+	cin >> adj[0];
+	cout << "The adjective you entered is " << adj[0] << "\n";
+
+	return 0;
+}
+
